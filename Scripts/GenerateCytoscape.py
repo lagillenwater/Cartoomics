8e82413f025e70446f27ce9b97e40629e1840b67
bsantan <70932395+bsantan@users.noreply.github.com>
2022-05-06 12:13:06 -0600

    Update GenerateCytoscape.py

--- Scripts/GenerateCytoscape.py
+++ Scripts/GenerateCytoscape.py
@@ -1,4 +1,4 @@
-#Input a sif file and a node attribute file to generate a cytoscape network visualization of the given mechanism. Sif file is of the format S (source node), P (predicate), O (Target node), "|" delimited, and noa file is of the format Node (PKL Node Label), Attribute (Mechanism, Extra). Must have cytoscape running and py4cytoscape installed.
+#Input an input directory with a sif file and a node attribute file to generate a cytoscape network visualization of the given mechanism. Sif file is of the format S (source node), P (predicate), O (Target node), "|" delimited, and noa file is of the format Node (PKL Node Label), Attribute (Mechanism, Extra). Must have cytoscape running and py4cytoscape installed.
 
 import pandas as pd
 import py4cytoscape as p4c
@@ -11,9 +11,8 @@ from py4cytoscape import palette_color_brewer_d_RdBu
 def defineArguments():
     parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--sif-file",dest="SifFile",required=True,help="SifFile")
-    parser.add_argument("--noa-file",dest="NoaFile",required=True,help="NoaFile")
-    parser.add_argument("--output-dir",dest="OutputDir",required=True,help="OutputDir")
+    parser.add_argument("--input-dir",dest="InputDir",required=True,help="InputDir")
+    parser.add_argument("--mechanism-filename",dest="MechanismFilename",required=True,help="MechanismFilename")
 
     return parser
 
@@ -24,13 +23,15 @@ def create_network(inputSif,inputNoa,filename):
     noa_df = pd.read_csv(inputNoa,sep='|')
     noa_df.columns = ['id','group']
 
-    p4c.create_network_from_data_frames(noa_df,sif_df,title='Mechanism')
+    p4c.create_network_from_data_frames(noa_df,sif_df,title=filename)
 
-    p4c.set_visual_style('Marquee',network='Mechanism')
+    p4c.set_visual_style('Marquee',network=filename)
 
     p4c.set_node_color_mapping(**gen_node_color_map('group', mapping_type='d',style_name='Marquee'))
+
+    #p4c.set_edge_label_mapping('interaction')
     
-    p4c.export_image(filename+'.png',network='Mechanism')
+    p4c.export_image(filename+'.png',network=filename)
 
 
 def main():
@@ -39,14 +40,13 @@ def main():
     parser = defineArguments()
     args = parser.parse_args()
     
+    input_dir = args.InputDir
+    mechanism_filename = args.MechanismFilename
 
-    sif_file = args.SifFile
-    noa_file = args.NoaFile
-    output_dir = args.OutputDir
+    sif_file = input_dir + '/' + mechanism_filename + '.csv'
+    noa_file = input_dir + '/' + mechanism_filename + '_attributes.noa'
 
-    filename = sif_file.split('.')[0]
-
-    create_network(sif_file,noa_file,filename)
+    create_network(sif_file,noa_file,mechanism_filename)
 
 if __name__ == '__main__':
     main()
