492aa7b1807cc4c4e110006091d2378ea0594df9
bsantan <70932395+bsantan@users.noreply.github.com>
2022-08-17 10:54:58 -0600

    Update Cartoomics_Generate_Graphs.py
    Added classes for prioritizing paths by cosine similarity or path degree product

--- Scripts/ObjectOriented_Testing/Cartoomics_Generate_Graphs.py
+++ Scripts/ObjectOriented_Testing/Cartoomics_Generate_Graphs.py
@@ -16,6 +16,16 @@ identifiers_file = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Outp
 
 triples_integers_file = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Output/PheKnowLator_v3.0.2_full_instance_relationsOnly_OWLNETS_Triples_node2vecInput_cleaned_withGutMGene_withMicrobes.txt'
 
+output_dir = '/Users/brooksantangelo/Documents/HunterLab/Cartoomics/Scripts/Test'
+
+node2vec_script_dir = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Scripts/' #/sparse_custom_node2vec_wrapper.py'
+
+embedding_dimensions = 42
+
+search_type = 'all'
+
+pdp_weight = 0.4
+
 
 # Created a class object
 graphObject = CreateGraph()
@@ -34,4 +44,23 @@ pathObject = ShortestPathSearch()
 
 df = pathObject.find_shortest_path_igraph('prostaglandin E synthase (human)','prostaglandin E2',g_igraph,g_nodes_igraph,identifiers,labels,triples_df,False,'all')
 
+print('shortest path result')
 print(df)
+
+dict_df,path_nodes = pathObject.find_all_shortest_paths_igraph('prostaglandin E synthase (human)','prostaglandin E2',g_igraph,g_nodes_igraph,identifiers,labels,triples_df,False,'all')
+
+print('all shortest paths result')
+print(dict_df)
+
+emb = pathObject.generate_graph_embeddings(triples_list_file,output_dir,node2vec_script_dir,embedding_dimensions)
+
+chosen_path_cs = pathObject.calc_cosine_sim(emb,path_nodes,g_nodes_igraph,identifiers,labels,triples_df,search_type)
+
+print('cosine similarity')
+print(chosen_path_cs)
+
+chosen_path_pdp = pathObject.calc_pdp(path_nodes,g_igraph,pdp_weight,g_nodes_igraph,identifiers,labels,triples_df,search_type)
+
+print('pdp')
+print(chosen_path_pdp)
+
