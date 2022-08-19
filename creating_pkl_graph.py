94e02970e4d51fa99347f935f9ad19dae751013a
bsantan <70932395+bsantan@users.noreply.github.com>
2022-08-19 09:40:01 -0600

    Update creating_pkl_graph.py
    Removed triples_integers_file and identifiers_file

--- creating_pkl_graph.py
+++ creating_pkl_graph.py
@@ -6,11 +6,7 @@ def main():
 
     labels_file = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Output/PheKnowLator_v3.0.2_full_instance_relationsOnly_OWLNETS_NodeLabels_NewEntities.txt'
 
-    identifiers_file = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Output/PheKnowLator_v3.0.2_full_instance_relationsOnly_OWLNETS_Triples_Integer_Identifier_Map_withGutMGene_withMicrobes.json'
-
-    triples_integers_file = '/Users/brooksantangelo/Documents/HunterLab/GutMGene_PKL/Output/PheKnowLator_v3.0.2_full_instance_relationsOnly_OWLNETS_Triples_node2vecInput_cleaned_withGutMGene_withMicrobes.txt'
-
-    g = create_pkl_graph(triples_list_file,labels_file,identifiers_file,triples_integers_file)
+    g = create_pkl_graph(triples_list_file,labels_file)
 
     print(len(g.edgelist))
 
