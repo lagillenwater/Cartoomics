49f973bd23a6d1ddb25d120188ea238121126f43
bsantan <70932395+bsantan@users.noreply.github.com>
2022-08-31 15:08:34 -0600

    Update create_graph.py
    remove brackets from URI in triples and labels files

--- create_graph.py
+++ create_graph.py
@@ -11,8 +11,16 @@ def process_pkl_files(triples_file,labels_file):
     triples_df = pd.read_csv(triples_file,sep = '	', quoting=csv.QUOTE_NONE)
     triples_df.columns.str.lower()
 
+    triples_df.replace({'<': ''}, regex=True, inplace=True)
+    triples_df.replace({'>': ''}, regex=True, inplace=True)
+
     labels = pd.read_csv(labels_file, sep = '	', quoting=csv.QUOTE_NONE)
     labels.columns.str.lower()
+
+    #Remove brackets from URI
+    labels['entity_uri'] = labels['entity_uri'].str.replace("<","")
+    labels['entity_uri'] = labels['entity_uri'].str.replace(">","")
+
 
     return triples_df,labels
 
