9a057a3c960f3587a3b9fd7a982f09be243b0d6b
bsantan <70932395+bsantan@users.noreply.github.com>
2022-09-11 15:29:08 -0600

    Create creating_subgraph_using_cosinesim.py
    Added existing creating_subgraph_using_cosinesim.py file back to not break the merging process

---
+++ creating_subgraph_using_cosinesim.py
@@ -0,0 +1,32 @@
+from inputs import *
+from create_graph import create_graph
+from assign_nodes import *
+from create_subgraph import subgraph_prioritized_path_cs
+from visualize_subgraph import output_visualization
+
+def main():
+
+    input_dir,output_dir,kg_type,embedding_dimensions,weights,search_type = generate_arguments()
+
+    triples_list_file,labels_file,input_file = get_graph_files(input_dir,output_dir, kg_type)
+    
+    print("Creating knowledge graph object from inputs.....")
+
+    g = create_graph(triples_list_file,labels_file, kg_type)
+
+    print("Mapping between user inputs and KG nodes.......")
+    
+    s = interactive_search_wrapper(g, input_file, output_dir)
+
+    print("Mapping complete")
+    print(s)
+    print("Finding subgraph using user input and KG embeddings......")
+    
+    subgraph = subgraph_prioritized_path_cs(s,g.igraph,g.igraph_nodes,g.labels_all,g.edgelist,weights,search_type,triples_list_file,output_dir,input_dir,embedding_dimensions)
+
+    print("Outputting visualization......")
+
+    output_visualization(s,subgraph,output_dir)
+
+if __name__ == '__main__':
+    main()
