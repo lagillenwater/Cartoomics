61f717afa088f5ef87ec7bf43537836166bd2447
lagillenwater <lagillenwater@gmail.com>
2023-09-19 16:33:16 -0600

    add pfocr_url as input

--- creating_subgraph_from_KG.py
+++ creating_subgraph_from_KG.py
@@ -11,7 +11,7 @@ def main():
 
     input_dir,output_dir,kg_type,embedding_dimensions,weights,search_type,pdp_weight,input_type = generate_arguments()
 
-    triples_list_file,labels_file,input_file = get_graph_files(input_dir,output_dir, kg_type,input_type)
+    triples_list_file,labels_file,input_file = get_graph_files(input_dir,output_dir, kg_type,input_type,pfocr_url)
 
     print("Creating knowledge graph object from inputs.....")
 
@@ -21,27 +21,27 @@ def main():
     
     s = interactive_search_wrapper(g, input_file, output_dir, input_type)
 
-    print("Mapping complete")
-    print(s)
+    # print("Mapping complete")
+    # print(s)
 
-    if weights == True:
-        g = user_defined_edge_exclusion(g,kg_type)
+    # if weights == True:
+    #     g = user_defined_edge_exclusion(g,kg_type)
 
-    print("Finding subgraph using user input and KG embeddings for Cosine Similarity......")
+    # print("Finding subgraph using user input and KG embeddings for Cosine Similarity......")
     
-    subgraph_cs,path_total_cs = subgraph_prioritized_path_cs(s,g.igraph,g.igraph_nodes,g.labels_all,g.edgelist,weights,search_type,triples_list_file,output_dir,input_dir,embedding_dimensions,kg_type)
+    # subgraph_cs,path_total_cs = subgraph_prioritized_path_cs(s,g.igraph,g.igraph_nodes,g.labels_all,g.edgelist,weights,search_type,triples_list_file,output_dir,input_dir,embedding_dimensions,kg_type)
 
-    print("Outputting CS visualization......")
+    # print("Outputting CS visualization......")
 
-    cs_noa_df = output_visualization(s,subgraph_cs,output_dir+'/CosineSimilarity')
+    # cs_noa_df = output_visualization(s,subgraph_cs,output_dir+'/CosineSimilarity')
 
-    print("Finding subgraph using user input for PDP......")
+    # print("Finding subgraph using user input for PDP......")
 
-    subgraph_pdp,path_pdp = subgraph_prioritized_path_pdp(s,g.igraph,g.igraph_nodes,g.labels_all,g.edgelist,weights,search_type,pdp_weight,output_dir,kg_type)
+    # subgraph_pdp,path_pdp = subgraph_prioritized_path_pdp(s,g.igraph,g.igraph_nodes,g.labels_all,g.edgelist,weights,search_type,pdp_weight,output_dir,kg_type)
     
-    print("Outputting PDP visualization......")
+    # print("Outputting PDP visualization......")
 
-    pdp_noa_df = output_visualization(s,subgraph_pdp,output_dir+'/PDP')
+    # pdp_noa_df = output_visualization(s,subgraph_pdp,output_dir+'/PDP')
 
 if __name__ == '__main__':
     main()
