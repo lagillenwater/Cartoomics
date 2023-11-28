12f7b5f0bb1e89f81511dad8784bae41c7b0711b
lagillenwater <lagillenwater@gmail.com>
2023-11-28 16:28:36 -0700

    script for converting wikipathways to graph format

---
+++ wikipathways_converter.py
@@ -0,0 +1,22 @@
+# from wikinetworks_mod import *
+import networkx as nx
+# import argparse
+
+# def define_arguments():
+#     parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#     ## Required inputs
+#     parser.add_argument("--wikipathway",dest="wikipathway",required=True,help="Wikipathways ID (e.g., WP5372), there is no default")
+#     return(parser)
+
+# parser = define_arguments()
+# args = parser.parse_args()
+# wikipathway = args.wikipathway
+
+
+# __all__ = ["WikiPathways"]
+# s = WikiPathways()
+
+# graph = runParsePathway(s, args.wikipathway)
+
+
+
