5cfebd4f1b788b900c509f0ab93ba823dbb5445b
lagillenwater <lagillenwater@gmail.com>
2024-01-18 13:03:36 -0700

    downloads graphs into specific file

--- wikipathways_converter.py
+++ wikipathways_converter.py
@@ -1,6 +1,7 @@
 from wikinetworks_mod import *
 import networkx as nx
 import argparse
+import os
 
 def define_arguments():
     parser=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -12,6 +13,15 @@ parser = define_arguments()
 args = parser.parse_args()
 wikipathway = args.wikipathway
 
+if not os.path.isdir("wikpathways_graphs"):
+    os.mkdir("wikipathways_graphs")
+
+os.chdir('./wikipathways_graphs')
+
+if not os.path.isdir(wikipathway):
+    os.mkdir(wikipathway)
+
+os.chdir(wikipathway)
 
 __all__ = ["WikiPathways"]
 s = WikiPathways()
