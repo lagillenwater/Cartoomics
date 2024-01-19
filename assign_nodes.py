ef4811e9aae921196a8bd41ae10267a3c16658d1
bsantan <70932395+bsantan@users.noreply.github.com>
2024-01-19 08:26:18 -0700

    Clarify message for inputting node entity_uri for duplicate labels.

--- assign_nodes.py
+++ assign_nodes.py
@@ -136,8 +136,11 @@ def manage_user_input(found_nodes,user_input,kg):
 			dup_node = True
 			logging.info('Duplicate label names found: %s',user_input)
 			while(dup_node):
-				user_id_input = input("Input node 'id': ")
-				print(found_nodes[found_nodes['label'] == user_input]['entity_uri'].values.tolist())
+				l = found_nodes[found_nodes['label'] == user_input]['entity_uri'].values.tolist()
+				print('Select from the following options: ')
+				for i in range(len(l)):
+					print(str(i+1),': ',l[i])
+				user_id_input = input("Input node 'entity_uri': ")
 				if user_id_input in found_nodes[found_nodes['label'] == user_input]['entity_uri'].values.tolist():
 					node_label = kg.labels_all.loc[kg.labels_all['entity_uri'] == user_id_input,'label'].values[0]
 					bad_input = False
