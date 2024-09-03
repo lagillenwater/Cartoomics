f005f3104a8003a50ad325ed7a3bbd3fbdc15f42
lagillenwater <lagillenwater@gmail.com>
2024-09-03 13:42:03 -0600

    created test using a biomedically trained RoBERTa model

---
+++ biomed_RoBERTa.py
@@ -0,0 +1,45 @@
+# from transformers import AutoModel
+# model = AutoModel.from_pretrained("allenai/biomed_roberta_base")
+
+
+from transformers import RobertaTokenizer, RobertaModel
+import torch
+from torch.nn.functional import cosine_similarity, pairwise_distance
+
+# Function to get embeddings
+def get_embeddings(model, tokenizer, sentence):
+    encoded_input = tokenizer(sentence, return_tensors='pt')
+    with torch.no_grad():
+        output = model(**encoded_input)
+    return output.last_hidden_state.mean(dim=1)
+
+# Initialize tokenizer and model
+tokenizer = RobertaTokenizer.from_pretrained('allenai/biomed_roberta_base')
+model = RobertaModel.from_pretrained('allenai/biomed_roberta_base')
+
+
+# Define sentences
+sentence1 = "SKI"
+sentence2 = "SMAD1"
+sentence3 = "PYROXD2"
+
+# # Define sentences
+# sentence1 = "NCBI:6497"
+# sentence2 = "NCBI:4086"
+# sentence3 = "NCBI:84795"
+
+
+# # Define sentences
+# sentence1 = "STAT1"
+# sentence2 = "IFNG"
+# sentence3 = "PYROXD2"
+
+# Get embeddings
+embeddings1 = get_embeddings(model, tokenizer, sentence1)
+embeddings2 = get_embeddings(model, tokenizer, sentence2)
+embeddings3 = get_embeddings(model, tokenizer, sentence3)
+
+# Calculate distances
+print("Cosine Distance between", sentence1, "and", sentence2, ":",  1 - cosine_similarity(embeddings1, embeddings2).item())
+print("Cosine Distance between", sentence1, "and", sentence3, ":",   1 - cosine_similarity(embeddings1, embeddings3).item())
+
