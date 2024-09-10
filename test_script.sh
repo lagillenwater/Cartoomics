8a0990a6b8f938794aded37f0e0108494ef717c2
lagillenwater <lagillenwater@gmail.com>
2024-09-10 13:42:35 -0500

    updated test script to include heatmap creation

--- test_script.sh
+++ test_script.sh
@@ -39,4 +39,8 @@
 #python wikipathways_idf_evaluations.py --knowledge-graph pkl --input-type annotated_diagram --wikipathways "['WP4532', 'WP4533', 'WP4534', 'WP4537', 'WP4538', 'WP4539', 'WP4542', 'WP4553', 'WP4562', 'WP4564', 'WP4565', 'WP4760', 'WP4856', 'WP5358', 'WP5368', 'WP5372', 'WP5373', 'WP5382', 'WP5385']" --enable-skipping True
 
 
-python wikipathways_literature_comparison_text.py --knowledge-graph pkl --input-type annotated_diagram --wikipathways "[ 'WP4533','WP4534']" --enable-skipping True
+## literature based NER comparison
+## python wikipathways_literature_comparison_text.py --knowledge-graph pkl --input-type annotated_diagram --wikipathways "[ 'WP4533','WP4534']" --enable-skipping True
+
+python wikipathways_literature_comparison_text.py --knowledge-graph pkl --input-type annotated_diagram --wikipathways "[ 'WP4533', 'WP4534', 'WP4537', 'WP4538', 'WP4539', 'WP4542', 'WP4553', 'WP4562']" --enable-skipping True
+Rscript literature_comparison_heatmap.R -i ./wikipathways_graphs/literature_comparison/Evaluation_Files/literature_comparison_evaluation_TEXT_abstract.csv -o ./wikipathways_graphs/literature_comparison/Evaluation_Files/heatmaps 
